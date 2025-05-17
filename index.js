761a7adfffb06c65f00399b0b638ff0427d2b357
Govind Singh <gs24746.gs@gmail.com>
2025-05-17 14:27:20 +0530

    added logger for more info

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const qrcode  = require('qrcode-terminal');
 const { Client  } = require('whatsapp-web.js');
 const cehckAuth = require('./src/middleware/auth');
+const logger = require('./logger');
 
 const app = express();
 const client = new Client({
@@ -18,6 +19,8 @@ client.on('qr', (qr) => {
     
     qrcode.generate(qr, { small: true });
     console.log('Scan QR Code !');
+    logger.info(`QR content: ${qr}`);
+
     
 });
 
@@ -30,6 +33,12 @@ client.on('message', msg => {
         msg.reply('pong');
     }
 });
+client.on('disconnected', (reason) => {
+    logger.info(`WhatsApp client disconnected: ${reason}`);
+});
+client.on('auth_failure', msg => {
+    logger.error(`Authentication failure: ${msg}`);
+});
 
 client.initialize();
 
@@ -37,11 +46,13 @@ app.post('/send-message', cehckAuth, async (req, res, next) => {
     try {
         const { number, message } = req.body;
         if (!number || !message) {
+             logger.warn('Missing number or message in request.');
             return res.json({msg:"number or message is missing"})
         }
         if (!client) throw new Error('WhatsApp client not initialized');
         const formattedNumber = number.includes('@c.us') ? number : `${number}@c.us`;
         await client.sendMessage(formattedNumber, message);
+        logger.info(`Message sent to ${number} (${formattedNumber})`);
         res.status(200).json({ success: true, message: `✅ Message sent to ${number}` });
     } catch (err) {
         next(err);
@@ -50,6 +61,7 @@ app.post('/send-message', cehckAuth, async (req, res, next) => {
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
+    logger.error(`Unhandled error: ${err.stack}`);
     res.status(500).json({ error: 'Internal Server Error' });
 });
 app.listen(process.env.PORT, () => {
---
+++ logger.js
@@ -0,0 +1,19 @@
+// logger.js
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(({ timestamp, level, message }) => {
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        })
+    ),
+    transports: [
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' }),
+        new transports.Console()
+    ],
+});
+
+module.exports = logger;
